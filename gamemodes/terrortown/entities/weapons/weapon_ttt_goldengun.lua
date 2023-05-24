2721234234f6a6288aabf4bfba734027af76e1d2
r47orr <razoroller529@gmail.com>
2023-05-24 08:59:04 -0300

    adicionar restrição de tempo para poder atirar
    * 1:30 de rodada necessário para atirar com a arma
    
    * mostra um aviso no chat caso seja muito cedo

--- gamemodes/terrortown/entities/weapons/weapon_ttt_goldengun.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_goldengun.lua
@@ -1,11 +1,11 @@
 if SERVER then
-	resource.AddWorkshop( "233067112" )
+   resource.AddWorkshop( "233067112" )
 end
 
 if CLIENT then
 
    SWEP.PrintName = "Golden Gun"
-   SWEP.Slot      = 6 -- add 1 to get the slot number key
+   SWEP.Slot      = 6
    SWEP.EquipMenuData = {
       type = "Arma",
       desc = "Atire em um traidor, mate o traidor. \nAtire em um inocente, se mate. \nSomente pode ser utilizada depois de 1:30 de rodada."
@@ -24,7 +24,7 @@ SWEP.Base        = "weapon_tttbase"
 
 SWEP.HoldType            = "pistol"
 
-SWEP.Primary.Delay       = 0.08
+SWEP.Primary.Delay       = 1
 SWEP.Primary.Recoil      = 1.2
 SWEP.Primary.Automatic   = true
 SWEP.Primary.Cone        = 0.025
@@ -92,9 +92,63 @@ local function GoldenGunDamage(attacker, trace, dmg)
    end
 end
 
+hook.Add('TTTBeginRound', "TTTGoldenGunTimerCheck", function()
+
+   if timer.Exists("GoldenGunTimerDelay") then
+      
+      timer.Remove("GoldenGunTimerDelay")
+
+      return
+   end
+
+   timer.Create("GoldenGunTimerDelay", 90, 0, function()
+
+      timer.Remove("GoldenGunTimerDelay")
+
+      return
+   end)
+end)
+
+function SWEP:CanPrimaryAttack()
+   if not IsValid(self:GetOwner()) then return end
+
+   if self:Clip1() <= 0 then
+
+      self:DryFire(self.SetNextPrimaryFire)
+
+      return false
+   end
+
+   return !timer.Exists("GoldenGunTimerDelay")
+end
 
 function SWEP:PrimaryAttack()
-   if !self:CanPrimaryAttack() then return end
+
+   if !self:CanPrimaryAttack() then
+      
+      if !timer.Exists(self:GetOwner():Nick() .. "_golden_message") then
+
+         if !timer.Exists("GoldenGunTimerDelay") then return end
+
+         local timeleft = math.floor(timer.TimeLeft("GoldenGunTimerDelay"))
+
+         -- since we are already running on the client, we don't need any checks here
+         chat.AddText(Color(230, 15, 15), "Você não pode utilizar a Golden Gun ainda! Espere mais " .. timeleft .. " segundo", (timeleft > 1 and "s!" or "!") )
+      
+         self:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
+         
+         timer.Create(self:GetOwner():Nick() .. "_golden_message", 5, 0, function() 
+            
+            timer.Remove(self:GetOwner():Nick() .. "_golden_message")
+
+            return
+         end)
+
+         return
+      end
+
+      return 
+   end
 
    self:TakePrimaryAmmo(1)
    self:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
